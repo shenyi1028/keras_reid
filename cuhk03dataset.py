c788b788c4b66bd441f3c66936e62ccfe8b797dd
Shen Yi <jerry31@qq.com>
2018-03-30 20:54:00 +0800

    edit cuhk03 API

--- cuhk03dataset.py
+++ cuhk03dataset.py
@@ -17,14 +17,16 @@ def get_img():
     query_labels = []
     query_labels.append(0)
 
-    test_img = cv2.imread("/Users/shenyi/Desktop/thesisCode/data/cuhk03_release/labeled/val/0001_05.jpg")
-    test_img = cv2.resize(test_img, (IMAGE_WIDTH, IMAGE_HEIGHT))
-    test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
-    test_img = np.reshape(test_img, (IMAGE_HEIGHT, IMAGE_WIDTH, 3)).astype(float)
     test_imgs = []
-    test_imgs.append(test_img)
-
     test_labels = []
-    test_labels.append(0)
+    for id in range(10):
+        test_img = cv2.imread(dataset_dir+"000"+str(id)+"_05.jpg")
+        test_img = cv2.resize(test_img, (IMAGE_WIDTH, IMAGE_HEIGHT))
+        test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+        test_img = np.reshape(test_img, (IMAGE_HEIGHT, IMAGE_WIDTH, 3)).astype(float)
+        test_imgs.append(test_img)
+
+        test_labels.append(id)
+        print(id)
     return np.asarray(query_imgs, np.float32),np.asarray(test_imgs, np.float32),\
            np.asarray(query_labels, np.int32),np.asarray(test_labels, np.int32)
